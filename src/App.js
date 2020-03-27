ac2dc0fc8023b59c7c6a3eb4735f8ec9db4cda29
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-03-27 19:44:23 +0100

    Add post and get methods to App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null)
+  const [uploading, setUploading] = useState(false)
+
+  const handleChange = e => {
+    const newImage = e.target.files[0];
+    setImage(newImage)
+  }
+
+  const handleSubmit = async () => {
+    setUploading(true)
+
+    const formData = new FormData()
+
+    
+    formData.append('addImg_file', image);
+    formData.append('addImg_dev', 5);
+
+    console.log('formData', formData)
+    
+    try {
+      const response = await fetch(`http://test2.inel.gda.pl/img.json`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Content-type': 'multipart/form-data',
+          'inel-token': 'dGVzdHxUZXN0VXNlcjF8dGVzdA==',
+          'Access-Control-Allow-Origin': '*'
+        },
+      })
+
+      const data = response.json()
+      console.log('data', data)
+      setUploading(false)
+
+    } catch (error) {
+
+      setUploading(false)
+      console.log('uploadImage error:', error)
+      
+    }
+    
+      
+  }
+
+  const handleDownload = async () => {
+    const resp = await fetch(`http://test2.inel.gda.pl/gimg.json`)
+    const data = resp.json()
+    console.log(data)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleSubmit}>Post</button>
+      <button onClick={handleDownload}>Get</button>
+      {uploading && <h1>Loading</h1>}
     </div>
   );
 }
